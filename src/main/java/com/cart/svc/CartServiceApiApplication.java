9f342ce00efbd82e6ddfe4d9d51f5d5c304b48c1
tsamudra <18121996DEs>
2022-02-16 19:18:00 +0700

    init commit

---
+++ src/main/java/com/cart/svc/CartServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.cart.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cart/svc/CartServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cart.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
